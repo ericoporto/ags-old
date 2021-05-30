2937556c841daef6ee11ab55f6769c56f23f33b4
ericoporto <ericoporto2008@gmail.com>
2021-05-30 10:25:27 -0700

    updates version and fixes to download.sh libsrc

--- libsrc/download.sh
+++ libsrc/download.sh
@@ -7,32 +7,33 @@ cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 function get {
     local URL=$1
-    local FILENAME=$(basename $URL)
+    local FILENAME=$2
     if [[ ! -f $FILENAME ]]; then
         echo
         echo ${FILENAME}: $URL
         echo
-        curl -O $URL
+        curl -L $URL --output ${FILENAME}
     fi
 }
 
-get https://s3-ap-southeast-2.amazonaws.com/ags-shared/libogg-1.3.2.tar.gz
-get http://downloads.xiph.org/releases/ogg/libogg-1.3.2.tar.gz
+LIBOGG_VERSION=1.3.4
+get https://github.com/xiph/ogg/releases/download/v${LIBOGG_VERSION}/libogg-${LIBOGG_VERSION}.tar.gz libogg-${LIBOGG_VERSION}.tar.gz
+get https://downloads.xiph.org/releases/ogg/libogg-${LIBOGG_VERSION}.tar.gz libogg-${LIBOGG_VERSION}.tar.gz
 
-get https://s3-ap-southeast-2.amazonaws.com/ags-shared/libvorbis-1.3.5.tar.gz
-get http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.5.tar.gz
+LIBVORBIS_VERSION=1.3.7
+get https://github.com/xiph/vorbis/releases/download/v${LIBVORBIS_VERSION}/libvorbis-${LIBVORBIS_VERSION}.tar.gz libvorbis-${LIBVORBIS_VERSION}.tar.gz
+get http://downloads.xiph.org/releases/vorbis/libvorbis-${LIBVORBIS_VERSION}.tar.gz libvorbis-${LIBVORBIS_VERSION}.tar.gz
 
-#git repo is definitely where new work happens
-#git clone https://git.xiph.org/theora.git
-get https://s3-ap-southeast-2.amazonaws.com/ags-shared/libtheora-20150828-gfbb2758.tar.bz2
-get https://s3-ap-southeast-2.amazonaws.com/ags-shared/libtheora-20160525-g50df933.tar.bz2
+LIBTHEORA_VERSION=1.1.1
+get https://github.com/xiph/theora/archive/refs/tags/v${LIBTHEORA_VERSION}.tar.gz libtheora-${LIBTHEORA_VERSION}.tar.gz
+# get https://downloads.xiph.org/releases/theora/libtheora-${LIBTHEORA_VERSION}.tar.bz2 libtheora-${LIBTHEORA_VERSION}.tar.bz2
 
 #new work in svn
 #svn co http://svn.xiph.org/trunk/Tremor/
 #git clone https://git.xiph.org/tremor.git
-# get https://s3-ap-southeast-2.amazonaws.com/ags-shared/libtremor-20150108-r19427.tar.bz2
+get https://s3-ap-southeast-2.amazonaws.com/ags-shared/libtremor-20150108-r19427.tar.bz2 libtremor-20150108-r19427.tar.bz2
 
-# get https://s3-ap-southeast-2.amazonaws.com/ags-shared/lua-5.1.5.tar.gz
-# get http://www.lua.org/ftp/lua-5.1.5.tar.gz
+get https://s3-ap-southeast-2.amazonaws.com/ags-shared/lua-5.1.5.tar.gz lua-5.1.5.tar.gz
+get http://www.lua.org/ftp/lua-5.1.5.tar.gz lua-5.1.5.tar.gz
 
 shasum --check sha1sums
