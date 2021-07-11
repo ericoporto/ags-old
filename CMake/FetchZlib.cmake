7f1f8850a12068dd8ae3023a5351638e9e61ae63
Érico Vieira Porto <ericoporto2008@gmail.com>
2021-07-10 22:05:23 -0300

    add zlib

---
+++ CMake/FetchZlib.cmake
@@ -0,0 +1,17 @@
+FetchContent_Declare(
+        zlib_content
+        URL https://github.com/madler/zlib/archive/cacf7f1d4e3d44d871b605da3b647f07d718623f.tar.gz
+        URL_HASH MD5=4df8ccfbd8f637d556f56da8ad348c19
+)
+
+FetchContent_GetProperties(zlib_content)
+if(NOT zlib_content_POPULATED)
+    FetchContent_Populate(zlib_content)
+
+    add_subdirectory(${zlib_content_SOURCE_DIR} ${zlib_content_BINARY_DIR} EXCLUDE_FROM_ALL)
+
+    set(ZLIB_LIBRARY zlib)
+    set(ZLIB_INCLUDE_DIR ${zlib_content_SOURCE_DIR})
+endif()
+
+
